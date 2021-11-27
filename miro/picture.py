779156807bd6c682d2e53ce539aa6f8f60fc2c6f
Hydroxide <anton.zolochevskiy@gmail.com>
2021-11-27 22:34:23 +0200

    Added Picture.parent __init__ will now remove "Optional[]" from picture id

--- miro/picture.py
+++ miro/picture.py
@@ -1,5 +1,8 @@
 import miro
+from . import base
 
-class Picture:
-    def __init__(self, data: dict):
-        pass
+class Picture(base.MiroObject):
+    def __init__(self, data: dict, parent):
+        super().__init__(data.get("id").lstrip("Optional[").rstrip("]"), parent.client, base.MiroObjectType.PICTURE)
+        self.parent = parent
+        self.url = data.get("imageUrl")
