e2a180c2a62e7441df890868abb5904caef29b85
Hydroxide <anton.zolochevskiy@gmail.com>
2021-11-27 20:33:58 +0200

    Fixed utils.parse_date datetime format

--- miro/utils.py
+++ miro/utils.py
@@ -3,7 +3,7 @@ import datetime
 
 def parse_date(datetimestr: str) -> datetime.datetime | None:
     try:
-        return datetime.datetime.strptime(datetimestr, '%Y-%m-%dT%H:%M:%S.%fZ')
+        return datetime.datetime.strptime(datetimestr, '%Y-%m-%dT%H:%M:%SZ')
     except ValueError:
         return None
 
