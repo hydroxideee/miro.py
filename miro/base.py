cdd6aac6021c1fbdf49c99188666ec8e0731cc68
Hydroxide <anton.zolochevskiy@gmail.com>
2021-11-27 21:56:36 +0200

    Added base class MiroObject and enum types MiroObjectType

---
+++ miro/base.py
@@ -0,0 +1,21 @@
+from typing import Type
+from enum import Enum
+
+class MiroObjectType(str,Enum):
+    TEAM = "team"
+    BOARD = "board"
+    WIDGET = "widget"
+    USER = "user"
+    PICTURE = "picture"
+
+    def __repr__(self) -> str:
+        return self.value
+
+class MiroObject:
+    def __init__(self,object_id: int | str, client, object_type: MiroObjectType):
+        self.id = int(object_id)
+        self.type = object_type
+        self.client = client
+
+    def __eq__(self, other):
+        return self.type == other.type and self.id == other.id
--- miro/team.py
+++ miro/team.py
@@ -1,17 +1,16 @@
 import miro
-from . import user, board, picture
+from . import user, base, picture
 from typing import List
 
-class Team:
+class Team(base.MiroObject):
     def __init__(self,data: dict, client):
-        self.id = data.get("id")
+        super().__init__(data.get("id"),client,base.MiroObjectType.TEAM)
         self.created_at = miro.utils.parse_date(data.get("createdAt",""))
         self.modified_at = miro.utils.parse_date(data.get("modifiedAt",""))
         self.created_by = user.User(data.get("createdBy"))
         self.modified_by = user.User(data.get("modifiedBy"))
         self.name = data.get("name")
         self.picture = picture.Picture(data.get("picture"))
-        self.client = client
     def get_team_users(self) -> List[user.TeamUser]:
         return [user.TeamUser(data) for data in self.client.get_request("/teams/{id}/user-connections",id=self.id)["data"]]
     def get_current_user(self) -> user.TeamUser:
