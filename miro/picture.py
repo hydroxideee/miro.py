8217f5ffb86514ef10245ff6faf21e58bf3a8f50
Hydroxide <anton.zolochevskiy@gmail.com>
2021-11-28 13:08:28 +0200

    Added Picture.save with the option to write to io.IOBase

--- miro/picture.py
+++ miro/picture.py
@@ -1,4 +1,6 @@
 import miro
+import io
+import requests
 from . import base
 
 
@@ -7,6 +9,20 @@ class Picture(base.MiroObject):
         super().__init__(data.get("id").lstrip("Optional[").rstrip("]"), parent.client, base.MiroObjectType.PICTURE)
         self.parent = parent
         self.url = data.get("imageUrl")
+
+    def save(self, fp, seek_start=True):
+        response = requests.get(self.url)
+        if not response.ok:
+            raise miro.HTTPException(response)
+        data = response.content
+        if isinstance(fp, io.IOBase) and fp.writable():
+            written = fp.write(data)
+            if seek_start:
+                fp.seek(0)
+            return written
+        else:
+            with open(fp, 'wb') as f:
+                return f.write(data)
 
     def __repr__(self):
         attrs = ("id", "url")
